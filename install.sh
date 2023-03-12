ae03b99ebec72a5fb24c2aa1c08b880d208e3171
mdibrahim1809 <116063220+mdibrahim1809@users.noreply.github.com>
2023-03-12 14:35:34 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,83 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is Made by IBRAHIM & MAHI "
+echo "---------------------------------------------------------------------------------"
+
+echo
+
+echo  "#####################################"
+echo   "  https://www.instagram.com/md.ibra.6244"               
+ echo   " https://facebook.com/md.ibra.6244"                                                                    
+echo   "####################################"
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  MY YOUTUBE CHANEL     "               
+ echo   " https://www.youtube.com/@a6244a "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  Follow Me On Instagram  "             
+ echo   " https://www.instagram.com/md.ibra.6244 "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE To My YOUTUBE CHANEL "               
+ echo   " https://www.youtube.com/@a6244a"                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo  "#####################################"
+echo   " MY PINTREST PROFILE "               
+ echo   " https://in.pinterest.com/ibrahimmondal1809/ "                                                                    
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  https://www.YouTube.com/@a6244a      "               
+ echo   " Subscribe Now "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  Follow Me On GitHub   "               
+ echo   " https://github.com/ibrahimmondal1809/ "                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
+
